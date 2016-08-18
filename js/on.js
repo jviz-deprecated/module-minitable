07b6c497bad4ba4e992616bd7e65c74f6d163ef9
jmjuanes <josemijuanes@gmail.com>
2016-08-18 16:43:11 +0200

    js/on.js: added events

--- js/on.js
+++ js/on.js
@@ -10,3 +10,23 @@ jviz.modules.tab.prototype.on = function(id, callback, scope)
   //Return this
   return this;
 };
+
+//Event when user clicks on the head
+jviz.modules.tab.prototype.onClickHead = function(index)
+{
+  //Show in console
+  console.log('Clicked on head. Column ' + index);
+  
+  //Send the event
+  jviz.events.send(this._events + 'click:head', this._columns.src[index], index);
+};
+
+//Event when user clicks on a row
+jviz.modules.tab.prototype.onClickRow = function(index)
+{
+  //Show in console
+  console.log('Clicked on row ' + index);
+
+  //Call the event listener
+  jviz.events.send(this._events + 'click:row', this._data.src[index], index);
+};
