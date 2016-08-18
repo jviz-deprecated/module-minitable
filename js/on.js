7bedfd0e4286106aa963c0613b8c6dbafc433d98
jmjuanes <josemijuanes@gmail.com>
2016-08-18 17:27:22 +0200

    js/on.js: added more events

--- js/on.js
+++ js/on.js
@@ -11,12 +11,22 @@ jviz.modules.tab.prototype.on = function(id, callback, scope)
   return this;
 };
 
+//Event when the table is drawn
+jviz.modules.tab.prototype.onDraw = function(start, end)
+{
+  //Show in console
+  console.log('Draw table [' + start + ' , ' + end + ']');
+
+  //Call the event
+  jviz.events.send(this._events + 'draw', start, end);
+}
+
 //Event when user clicks on the head
 jviz.modules.tab.prototype.onClickHead = function(index)
 {
   //Show in console
   console.log('Clicked on head. Column ' + index);
-  
+
   //Send the event
   jviz.events.send(this._events + 'click:head', this._columns.src[index], index);
 };
@@ -29,4 +39,24 @@ jviz.modules.tab.prototype.onClickRow = function(index)
 
   //Call the event listener
   jviz.events.send(this._events + 'click:row', this._data.src[index], index);
+};
+
+//Event when user select a row
+jviz.modules.tab.prototype.onSelectRow = function(index)
+{
+  //Show in console
+  console.log('Selected row ' + index);
+
+  //Call the event listener
+  jviz.events.send(this._events + 'select:row', this._data.src[index], index);
+};
+
+//Event when user un-select a row
+jviz.modules.tab.prototype.onUnSelectRow = function(index)
+{
+  //Show in console
+  console.log('Unselected row ' + index);
+
+  //Call the event listener
+  jviz.events.send(this._events + 'unselect:row', this._data.src[index], index);
 };
