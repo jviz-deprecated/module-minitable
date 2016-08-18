88e13a2aa929546285fc61f1f60c7170ee340590
jmjuanes <josemijuanes@gmail.com>
2016-08-18 16:43:28 +0200

    js/head.js: updated head events

--- js/head.js
+++ js/head.js
@@ -20,27 +20,16 @@ jviz.modules.tab.prototype.head = function()
     jviz.dom.append({ type: 'div', id: id, class: this._head.cell.class, html: cell.title }, this._head.row.id);
   }
 
+  //Save this
+  var self = this;
+
   //Add all the events
-  for(var i = 0; i < this._columns.src.length; i++)
+  for(let i = 0; i < this._columns.src.length; i++)
   {
     //Check for orderable
     if(this._columns.src[i].orderable === false){ continue; }
 
     //Add the event for this column
-    jvizModulesTabHeadEvent(this, i);
+    $('#' + self._head.cell.id + i).click(function(e){ return self.onClickHead(i); });
   }
 };
-
-//Head cell click
-jviz.modules.tab.prototype.headClick = function(index)
-{
-  //Send the event
-  jviz.events.send(this._events + 'click:head', this._columns.src[index], index);
-};
-
-//Head cell click events
-function jvizModulesTabHeadEvent(_this, _index)
-{
-  //Add the event
-  $('#' + _this._head.cell.id + _index).click(function(e){ return _this.headClick(_index); });
-}
