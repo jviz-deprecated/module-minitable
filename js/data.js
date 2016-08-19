b22165d2637647032c8221f05e123838f46c4b6a
jmjuanes <josemijuanes@gmail.com>
2016-08-19 13:01:06 +0200

    js/data.js: parse data

---
+++ js/data.js
@@ -0,0 +1,24 @@
+//Display the data
+jviz.modules.tab.prototype.data = function(data)
+{
+  //Check for undefined data
+  if(typeof data === 'undefined'){ return this; }
+
+  //Check for array
+  if(jviz.array.is(data) === false){ data = [ data ]; }
+
+  //Update the data
+  this._data.src = data;
+
+  //Save the data count
+  this._data.length = data.length;
+
+  //Reset the filter
+  this.filterClear();
+
+  //Reset the order
+  this.orderClear();
+
+  //Return this
+  return this;
+};
