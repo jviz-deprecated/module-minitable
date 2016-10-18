3e1580590784e5c8ceddd58b9771d06a826e4adf
jmjuanes <josemijuanes@gmail.com>
2016-10-18 16:26:19 +0200

    js/on.js: updated events

--- js/on.js
+++ js/on.js
@@ -1,25 +1,12 @@
 //Register an event
-jviz.modules.tab.prototype.on = function(id, callback, scope)
+jviz.modules.tab.prototype.on = function(name, callback)
 {
-  //Check the id
-  if(id.replace(/\s/g, '') === ''){ return this; }
-
   //Add the event
-  jviz.events.add(this._events + id, callback, scope);
+  this._events.add(id, callback);
 
   //Return this
   return this;
 };
-
-//Event when the table is drawn
-jviz.modules.tab.prototype.onDraw = function(start, end)
-{
-  //Show in console
-  console.log('Draw table [' + start + ' , ' + end + ']');
-
-  //Call the event
-  jviz.events.send(this._events + 'draw', start, end);
-}
 
 //Event when user clicks on the head
 jviz.modules.tab.prototype.onClickHead = function(index)
@@ -28,7 +15,7 @@ jviz.modules.tab.prototype.onClickHead = function(index)
   console.log('Clicked on head. Column ' + index);
 
   //Send the event
-  jviz.events.send(this._events + 'click:head', this._columns.src[index], index);
+  this._events.emit('click:head', this._columns.src[index], index);
 };
 
 //Event when user clicks on a row
@@ -38,25 +25,32 @@ jviz.modules.tab.prototype.onClickRow = function(index)
   console.log('Clicked on row ' + index);
 
   //Call the event listener
-  jviz.events.send(this._events + 'click:row', this._data.src[index], index);
+  this._events.emit('click:row', this._data.src[index], index);
 };
 
-//Event when user select a row
-jviz.modules.tab.prototype.onSelectRow = function(index)
+//Event when the user clicks on a cell
+jviz.modules.tab.prototype.onClickCell = function(index_row, index_cell)
 {
-  //Show in console
-  console.log('Selected row ' + index);
-
-  //Call the event listener
-  jviz.events.send(this._events + 'select:row', this._data.src[index], index);
+  //Call the event
+  this._events.emit('click:cell');
 };
 
-//Event when user un-select a row
-jviz.modules.tab.prototype.onUnSelectRow = function(index)
+//Event when user checks a row
+jviz.modules.tab.prototype.onCheckedRow = function(index)
 {
   //Show in console
-  console.log('Unselected row ' + index);
+  console.log('Checked row ' + index);
 
   //Call the event listener
-  jviz.events.send(this._events + 'unselect:row', this._data.src[index], index);
+  this._events.emit('checked:row', this._data.src[index], index);
+};
+
+//Event when user un-check a row
+jviz.modules.tab.prototype.onUnCheckedRow = function(index)
+{
+  //Show in console
+  console.log('Unchecked row ' + index);
+
+  //Call the event listener
+  this._events.emit('unchecked:row', this._data.src[index], index);
 };
