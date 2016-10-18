f8f10cf89f13c5d8c2df9de63e4333dab161362e
jmjuanes <josemijuanes@gmail.com>
2016-10-18 21:33:29 +0200

    js/checked.js: manage checked rows

---
+++ js/checked.js
@@ -0,0 +1,29 @@
+//Check a row
+jviz.modules.tab.prototype.check = function(index)
+{
+  //
+};
+
+//Check all rows
+jviz.modules.tab.prototype.checkAll = function()
+{
+  //
+};
+
+//Uncheck a row
+jviz.modules.tab.prototype.uncheck = function(index)
+{
+  //
+};
+
+//Uncheck all rows
+jviz.modules.tab.prototype.uncheckAll = function()
+{
+  //
+};
+
+//Get checked rows
+jviz.modules.tab.prototype.checked = function()
+{
+
+};
