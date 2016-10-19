0ce800ee01ec7e1720045bd172cd3920aac030c2
jmjuanes <josemijuanes@gmail.com>
2016-10-19 15:15:57 +0200

    js/columns.js: build head checkbox

--- js/columns.js
+++ js/columns.js
@@ -2,13 +2,16 @@
 jviz.modules.tab.prototype.columns = function(list)
 {
   //Check the columns list
-  if(typeof list === 'undefined'){ var list = this._columns.src; }
+  if(typeof list === 'undefined'){ return this._columns.src; }
 
   //Append the head row
   jviz.dom.html(this._head.id, { _tag: 'div', id: this._head.row.id, class: this._head.row.class });
 
   //Save the list
   this._columns.src = this.parseColumns(list);
+
+  //Check the size
+  if(this._columns.src.length === 0){ return this; }
 
   //Read all the columns
   for(var i = 0; i < this._columns.src.length; i++)
@@ -19,11 +22,33 @@ jviz.modules.tab.prototype.columns = function(list)
     //Check for display
     if(cell.display === false){ continue; }
 
-    //Build the cell id
-    var id = this._head.cell.id + i;
+    //Get the cell id
+    var cell_id = this._head.cell.id + i;
+
+    //Get the cell class
+    var cell_class = this._head.cell.class;
 
     //Add the cell
-    jviz.dom.append(this._head.row.id, { _tag: 'div', id: id, class: this._head.cell.class, _html: cell.title });
+    jviz.dom.append(this._head.row.id, { id: cell_id, class: cell_class });
+
+    //Check for checkbox column
+    if(cell.type === 'checkbox')
+    {
+      //Set enabled
+      this._check.enabled = true;
+
+      //Add the check cell class
+      jviz.dom.class.add(cell_id, this._head.cell.check.class);
+
+      //Create the checkbox
+      this._check.head = new jviz.components.checkbox({ parent: cell_id, class: this._check.class });
+
+      //Continue
+      continue;
+    }
+
+    //Default, add the cell title
+    jviz.dom.html(cell_id, cell.title);
   }
 
   //Save this
@@ -35,59 +60,76 @@ jviz.modules.tab.prototype.columns = function(list)
     //Check for orderable
     if(this._columns.src[i].orderable === false){ continue; }
 
+    //Check for checkbox column
+    if(this._columns.src[i].type === 'checkbox'){ continue; }
+
     //Add the event for this column
-    $('#' + self._head.cell.id + i).click(function(e){ return self.onClickHead(i); });
+    $('#' + self._head.cell.id + i).click(function(e){ return self.columnsClick(i); });
   }
 
   //Return this
   return this;
 };
 
+//Column head click
+jviz.modules.tab.prototype.columnsClick = function(index)
+{
+  //Show in console
+  console.log('Clicked on head. Column ' + index);
+
+  //Send the event
+  this._events.emit('click:head', this._columns.src[index], index);
+};
+
 //Parse a columns list
 jviz.modules.tab.prototype.parseColumns = function(list)
 {
+  //Check the list
+  if(jviz.is.array(list) === false){ list = [ list ]; }
+
+  //Output list
+  var out = [];
+
+  //Flags
+  var has_checkbox = false;
+
   //Read all the columns
   for(var i = 0; i < list.length; i++)
   {
+    //Get the list element
+    var el = list[i];
+
+    //Check the column type
+    if(typeof el.type === 'undefined'){ el.type = 'default'; }
+
+    //Check the column type value
+    if(this._columns.type.indexOf(el.type) === -1){ el.type = 'default'; }
+
+    //Check for checkbox
+    if(el.type === 'checkbox' && has_checkbox === true){ console.error('Only one checkbox column is allowed'); continue; }
+
+    //Check for checkbox
+    if(el.type === 'checkbox'){ has_checkbox = true; }
+
     //Check the key
-    if(typeof list[i].key === 'undefined'){ console.error('jviz-tab: undefined key on column ' + i); return []; }
+    if(typeof el.key === 'undefined'){ el.key = ''; }
 
     //Check the title
-    if(typeof list[i].title === 'undefined'){ list[i].title = list[i].key; }
+    if(typeof el.title === 'undefined'){ el.title = el.key; }
 
     //Check the display
-    if(typeof list[i].display === 'undefined'){ list[i].display = true; }
+    if(typeof el.display === 'undefined'){ el.display = true; }
 
     //Check the orderable attribute
-    if(typeof list[i].orderable === 'undefined'){ list[i].orderable = true; }
+    if(typeof el.orderable === 'undefined'){ el.orderable = true; }
 
     //Check the column parse function
-    if(typeof list[i].parse === 'undefined'){  }
+    if(typeof el.parse === 'undefined'){  }
+
+    //Save the list element
+    out.push(el);
   }
 
-  //Return the parssed list
-  return list;
-};
-
-//Get columns
-jviz.modules.tab.prototype.getColumns = function(list)
-{
-  //Check the index
-  if(typeof list === 'undefined'){ return this._columns.src; }
-
-  //Check for array
-  if(jviz.is.array(list) === false){ list = [ list ]; }
-
-  //Output columns
-  var out = [];
-
-  //Get all columns
-  for(var i = 0; i < list.length; i++)
-  {
-    //Save the column
-    out.push(this._columns.src[list[i]]);
-  }
-
-  //Return the columns
+  //Return the parsed list
   return out;
 };
