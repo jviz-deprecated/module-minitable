bb7ec4613f7adbfbcca60993fc21c993bddbaf84
jmjuanes <josemijuanes@gmail.com>
2016-10-19 16:32:25 +0200

    js/check.js: check and uncheck a row. Add checkboxes events

--- js/check.js
+++ js/check.js
@@ -1,7 +1,17 @@
 //Check a row
 jviz.modules.tab.prototype.check = function(index)
 {
-  //
+  //Update the check list
+  this._data.check[index] = true;
+
+  //Emit the check event
+  this._events.emit('check:row', this._data.src[index], index);
+
+  //Check if this checkbox exists
+  if(typeof this._check.el[index] === 'undefined'){ return; }
+
+  //Check this element
+  this._check.el[index].checked(true);
 };
 
 //Check all rows
@@ -13,7 +23,17 @@ jviz.modules.tab.prototype.checkAll = function()
 //Uncheck a row
 jviz.modules.tab.prototype.uncheck = function(index)
 {
-  //
+  //Update the check list
+  this._data.check[index] = false;
+
+  //Emit the uncheck event
+  this._events.emit('uncheck:row', this._data.src[index], index);
+
+  //Check if this checkbox exists
+  if(typeof this._check.el[index] === 'undefined'){ return; }
+
+  //Check this element
+  this._check.el[index].checked(false);
 };
 
 //Uncheck all rows
@@ -50,4 +70,24 @@ jviz.modules.tab.prototype.clearCheck = function()
 {
   //Remove all checks
   this._data.check = jviz.math.array.create(this._data.length, false);
+};
+
+//Check event
+jviz.modules.tab.prototype.checkEvent = function(index)
+{
+  //Save this
+  var self = this;
+
+  //Get the checkbox element
+  var el = this._check.el[index];
+
+  //Add the change event
+  el.on('change', function()
+  {
+    //Get the actual status
+    var status = el.checked();
+
+    //Check or uncheck the checkbox
+    (status === true) ? self.check(index) : self.uncheck(index);
+  });
 };
