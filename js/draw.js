cab623cd6a9e2a9d873f14a92da639a579a893a5
jmjuanes <josemijuanes@gmail.com>
2016-10-19 15:29:05 +0200

    js/draw.js: build rows checkboxes

--- js/draw.js
+++ js/draw.js
@@ -14,16 +14,16 @@ jviz.modules.tab.prototype.draw = function(subset)
   var max_size = this._data.length - 1;
 
   //Parse the subset start point
-  subset.start = (typeof subset.start === 'undefined') ? 0 : Math.max(0, parseInt(subset.start));
+  this._draw.start = (typeof subset.start === 'undefined') ? 0 : Math.max(0, parseInt(subset.start));
 
   //Parse the subset end point
-  subset.end = (typeof subset.end === 'undefined') ? max_size : Math.min(max_size, parseInt(subset.end));
+  this._draw.end = (typeof subset.end === 'undefined') ? max_size : Math.min(max_size, parseInt(subset.end));
 
   //Array with the added rows
   var rows = [];
 
   //Display the data
-  for(var i = subset.start; i <= subset.end; i++)
+  for(var i = this._draw.start; i <= this._draw.end; i++)
   {
     //Get the index
     var index = this._data.order[i];
@@ -49,32 +49,59 @@ jviz.modules.tab.prototype.draw = function(subset)
       //Get the cell id
       var cell_id = this._body.cell.id + index + '_' + j;
 
+      //Create the cell
+      jviz.dom.append(row_id, { _tag: 'div', id: cell_id, class: this._body.cell.class });
+
+      //Check the cell type
+      if(cell.type === 'checkbox')
+      {
+        //Add the check cell class
+        jviz.dom.class.add(cell_id, this._body.cell.check.class);
+
+        //Build the checkbox
+        var cell_box = new jviz.components.checkbox({ parent: cell_id, class: this._check.class });
+
+        //Continue
+        continue;
+      }
+
       //Get the content
       var content = (typeof cell.parse === 'function') ? cell.parse(cell.key, data, index, cell) : data[cell.key];
 
-      //Create the cell
-      jviz.dom.append(row_id, { _tag: 'div', id: cell_id, class: this._body.cell.class, _html: content });
+      //Add the content
+      jviz.dom.html(cell_id, content);
     }
 
-    //Add the row index
+    //Check the row class
+    if(typeof this._data.class[index] !== 'undefined')
+    {
+      //Add the class to this row
+      for(var j = 0; j < this._data.class[index].length; i++)
+      {
+        //Add the row class
+        jviz.dom.class.add(row_id, this._data.class[index][j]);
+      }
+    }
+
+    //Save the row index and ID
     rows.push({ id: row_id, index: index });
   }
 
-  //Save this
-  var self = this;
-
   //Add the events
-  for(let i = 0; i < rows.length; i++)
+  for(var i = 0; i < rows.length; i++)
   {
-    //Add the event
-    $('#' + rows[i].id).on('click', function(e){ return self.onClickRow(rows[i].index); });
+    //Add the row event
+    this.rowsEvent(rows[i].index);
+
+    //Add the checkbox event
+    this.checkEvent(rows[i].index);
   }
 
   //Show in console
-  jviz.console.log('Draw table [' + subset.start + ' , ' + subset.end + ']');
+  jviz.console.log('Draw table [' + this._draw.start + ' , ' + this._draw.end + ']');
 
   //Emit the event
-  this._events.emit('draw', subset.start, subset.end);
+  this._events.emit('draw', this._draw.start, this._draw.end);
 
   //Return this
   return this;
