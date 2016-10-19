9883d009d629f24b9fba07dc8a3c657fc2db93f1
jmjuanes <josemijuanes@gmail.com>
2016-10-19 08:59:02 +0200

    js/rows.js: add or remove a row class

--- js/rows.js
+++ js/rows.js
@@ -1,5 +1,5 @@
 //Get the selected rows data
-jviz.modules.tab.prototype.getRows = function(sel)
+jviz.modules.tab.prototype.rows = function(sel)
 {
   //Check the selection
   if(typeof sel === 'undefined'){ return this._data.src; }
@@ -30,8 +30,52 @@ jviz.modules.tab.prototype.getRows = function(sel)
   return out;
 };
 
-//Return only the selected rows
-jviz.modules.tab.prototype.getRowsSelected = function()
+//Return only the checked rows
+jviz.modules.tab.prototype.selectedRows = function()
 {
 
+};
+
+//Set the row background color
+jviz.modules.tab.prototype.rowBg = function(index, color)
+{
+  //
+};
+
+//Clear the row background color
+jviz.modules.tab.prototype.rowBgClear = function(index)
+{
+  //
+};
+
+//Add a class to the row
+jviz.modules.tab.prototype.addRowClass = function(index, name)
+{
+  //Check for undefined class name
+  if(typeof name === 'undefined'){ return this; }
+
+  //Get the row id
+  var id = this._body.row.id + index;
+
+  //Add the class to the row
+  jviz.dom.class.add(id, name);
+
+  //Return this
+  return this;
+};
+
+//Remove a class from the specified row
+jviz.modules.tab.prototype.removeRowClass = function(index, name)
+{
+  //Check for undefined class name
+  if(typeof name === 'undefined'){ return this; }
+
+  //Get the row id
+  var id = this._body.row.id + index;
+
+  //Add the class to the row
+  jviz.dom.class.remove(id, name);
+
+  //Return this
+  return this;
 };
