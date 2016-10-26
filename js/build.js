39ebafa962297b184dd110a4711fc061cc200632
jmjuanes <josemijuanes@gmail.com>
2016-10-26 16:01:23 +0200

    js/build.js: build table

--- js/build.js
+++ js/build.js
@@ -1,14 +1,17 @@
 //Build the table
 jviz.modules.tab.prototype.build = function()
 {
+  //Append the table container
+  jviz.dom.append(this._parent, { id: this._id, class: this._class });
+
   //Append the table
-  jviz.dom.append(this._parent, { _tag: 'div', id: this._id, class: this._class });
+  jviz.dom.append(this._id, { id: this._table.id, class: this._table.class });
 
   //Append the table head
-  jviz.dom.append(this._id, { _tag: 'div', id: this._head.id, class: this._head.class });
+  jviz.dom.append(this._table.id, { id: this._head.id, class: this._head.class });
 
   //Append the table body
-  jviz.dom.append(this._id, { _tag: 'div', id: this._body.id, class: this._body.class });
+  jviz.dom.append(this._table.id, { id: this._body.id, class: this._body.class });
 
   //Parse and build the columns
   this.columns(this._columns.src);
